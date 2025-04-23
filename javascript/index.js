b1e5350089a7c12ba9f6e17c1f505f0c6eba736e
ArpitArya29 <arpitaryan48@gmail.com>
2025-04-23 15:17:26 +0530

    Image background changing file

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+// var a = 20;
+// console.log(a);
+
+
+// // hoisting
+// bla = 2;
+// // var bla;
+// console.log(bla);
+// console.log(typeof(bla));
+
+
+var a=1;
+var a=2;
+console.log(a);
+var a;
+console.log(a);
